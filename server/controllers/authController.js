aac479537c3a9640ef1be75967ade15b5e8b92e1
Aman Mishra <amanmishra.5272@gmail.com>
2025-06-05 22:36:39 +0530

    feat(auth): implement logout functionality to clear JWT cookie

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -69,6 +69,19 @@ export const login = async (req,res) =>{
         })
         res.json({success:true, message: "Login successful"});
     } catch (error) {
-        
+        return res.json({success:false, message: error.message});
+    }
+}
+
+export const logout = async (req , res) => {
+    try {
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production'? 'none' :'strict'   
+        })
+        return res.json({success:true, message: "Logout successful"});
+    } catch (error) {
+        return res.json({success:false, message: error.message});
     }
 }
