f97740939cda377230c8d8e9196237d797faffe3
Aman Mishra <amanmishra.5272@gmail.com>
2025-06-05 22:27:39 +0530

    feat(auth): implement login functionality with JWT authentication

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -35,8 +35,40 @@ export const register = async (req, res) => {
             sameSite: process.env.NODE_ENV === 'production'? 'none' :'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
+        res.json({success:true, message: "User registered successfully"});
 
     } catch (error) {
         return res.json({success:false, message: error.message});
+    }
+}
+
+export const login = async (req,res) =>{
+    const {email, password} = req.body;
+    if(!email || !password) {
+        return res.json({success:false, message: "Credentials are required"});
+    }
+    try {
+        const user = await userModel.findOne({email});
+        if(!user) {
+            return res.json({success:false, message: "User not found"});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch) {
+            return res.json({success:false, message: "Invalid credentials"});
+        }
+
+        // Generate JWT token
+        const token = jwt.sign({id:user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
+
+        res.cookie('token',token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production'? 'none' :'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
+        res.json({success:true, message: "Login successful"});
+    } catch (error) {
+        
     }
 }
