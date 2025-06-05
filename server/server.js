1e42490667eca68452b52161bc04039406a48ee8
Aman Mishra <amanmishra.5272@gmail.com>
2025-06-05 23:18:59 +0530

    feat(auth): refactor authentication routes and update import paths

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 import connectDB from './config/mongodb.js';
+import authRouter from './routes/authRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -12,9 +13,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials:true}));
 
+// API Routes
 app.get('/', (req, res) => {
   res.send('Welcome to the server!');
 });
+app.use('/api/auth', authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
