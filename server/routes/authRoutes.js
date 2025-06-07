2597d3cd68999473ed29cfd5f38190721154918a
Aman Mishra <amanmishra.5272@gmail.com>
2025-06-07 14:39:29 +0530

    feat(auth): add routes for password reset and OTP sending

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,13 +1,16 @@
 import express from 'express';
-import { isAuthenticated, login, logout, register, sendVerifyOtp ,veifyEmail} from '../controllers/authController.js';
+import { isAuthenticated, login, logout, register, resetPassword, sendResetOtp, sendVerifyOtp ,veifyEmail} from '../controllers/authController.js';
 import userAuth from '../middleware/userAuth.js';
 
 const authRouter = express.Router();
 authRouter.post('/register', register);
 authRouter.post('/login', login);
 authRouter.post('/logout', logout);
-authRouter.post('/send-otp', userAuth, sendVerifyOtp);
+authRouter.post('/send-verify-otp', userAuth, sendVerifyOtp);
 authRouter.post('/verify-account', userAuth, veifyEmail);
 authRouter.post('/is-auth',userAuth, isAuthenticated);
+authRouter.post('/send-reset-otp', sendResetOtp);
+authRouter.post('/reset-password', resetPassword);
+
 
 export default authRouter;
