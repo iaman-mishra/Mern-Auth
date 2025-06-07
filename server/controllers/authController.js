0314c1e7ade281504cd19051a475c88eac7fecb8
Aman Mishra <amanmishra.5272@gmail.com>
2025-06-07 13:58:38 +0530

    feat(auth): add isAuthenticated function to verify user authentication

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -157,3 +157,11 @@ export const veifyEmail = async (req, res) => {
     }
 }
 
+// This function checks if the user is authenticated
+export const isAuthenticated = async (req,res)=>{
+    try {
+        return res.json({success:true});
+    } catch (error) {
+        return res.json({success:false, message: error.message});
+    }
+}
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { login, logout, register, sendVerifyOtp ,veifyEmail} from '../controllers/authController.js';
+import { isAuthenticated, login, logout, register, sendVerifyOtp ,veifyEmail} from '../controllers/authController.js';
 import userAuth from '../middleware/userAuth.js';
 
 const authRouter = express.Router();
@@ -7,6 +7,7 @@ authRouter.post('/register', register);
 authRouter.post('/login', login);
 authRouter.post('/logout', logout);
 authRouter.post('/send-otp', userAuth, sendVerifyOtp);
-authRouter.post('/verify-account', userAuth, veifyEmail)
+authRouter.post('/verify-account', userAuth, veifyEmail);
+authRouter.post('/is-auth',userAuth, isAuthenticated);
 
 export default authRouter;
