ca0fa7ba478488c89c523b95f0e89ab09038e675
Aman Mishra <amanmishra.5272@gmail.com>
2025-06-07 13:04:51 +0530

    feat(auth): implement user authentication middleware with JWT verification

---
+++ server/middleware/userAuth.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+
+const userAuth = (req, res, next)=>{
+    const token = req.cookies.token;
+    if(!token) {
+        return res.json({success: false, message: "Unauthorized access try to login again"});
+    }
+    try {
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        if (decodedToken.id) {
+            req.body.userId = decodedToken.id; // Attaching user ID to request body
+        }else{
+            return res.json({success: false, message: "Unauthorized access try to login again"});
+        }
+        next();
+    } catch (error) {
+        return res.json({success: false, message: error.message});
+    }
+}
