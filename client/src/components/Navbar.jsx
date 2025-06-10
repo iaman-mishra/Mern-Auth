165b2d10a620b5209a9ee251c662afbbeb1a231e
Aman Mishra <amanmishra.5272@gmail.com>
2025-06-10 23:51:54 +0530

    feat: enhance Navbar component to display user information and provide navigation options based on authentication status

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
-import {assets} from '../assets/assets.js'
-import {useNavigate} from 'react-router-dom'
+import React, { useContext } from "react";
+import { assets } from "../assets/assets.js";
+import { useNavigate } from "react-router-dom";
+import { AppContext } from "../context/AppContext.jsx";
 const Navbar = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const { userData, backendUrl, setUserData, setIsLoggedIn } = useContext(AppContext);
   return (
-    <div className='flex w-full justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0'>
-        <img src={assets.logo} alt="" className='w-28 sm:w-32' />
-        <button onClick={()=> navigate('/login')} className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all'>Login <img src={assets.arrow_icon} alt="" /></button>
+    <div className="flex w-full justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0">
+      <img src={assets.logo} alt="" className="w-28 sm:w-32" />
+      {userData ? (
+        <div className="w-8 h-8 flex justify-center items-center rounded-full bg-black text-white relative group">
+          {userData.name[0].toUpperCase()}
+          <div className="absolute top-0 right-0 hidden group-hover:block z-10 text-black rounded pt-10 ">
+            <ul className="list-none m-0 p-2 bg-gray-100 text-sm">
+              <li className="py-1 px-2 hover:bg-gray-200 cursor-pointer">Verify Email</li>
+              <li className="py-1 px-2 pr-10 hover:bg-gray-200 cursor-pointer">Logout</li>
+            </ul>
+          </div>
+        </div>
+      ) : (
+        <button
+          onClick={() => navigate("/login")}
+          className="flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all"
+        >
+          Login <img src={assets.arrow_icon} alt="" />
+        </button>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
