d7fbfb500e0d5208b269b4612711385af6cd7cef
Aman Mishra <amanmishra.5272@gmail.com>
2025-06-11 01:13:48 +0530

    feat: enhance Navbar with logout functionality and email verification prompt

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,9 +2,27 @@ import React, { useContext } from "react";
 import { assets } from "../assets/assets.js";
 import { useNavigate } from "react-router-dom";
 import { AppContext } from "../context/AppContext.jsx";
+import axios from "axios";
+import { toast } from "react-toastify";
 const Navbar = () => {
   const navigate = useNavigate();
   const { userData, backendUrl, setUserData, setIsLoggedIn } = useContext(AppContext);
+
+  const handleLogout = async (req, res)=>{
+    try {
+      axios.defaults.withCredentials = true;
+      const {data} = await axios.post(backendUrl + "/api/auth/logout");
+      if (data.success) {
+        toast.success(data.message);
+        setUserData(null);
+        setIsLoggedIn(false);
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }
+
   return (
     <div className="flex w-full justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0">
       <img src={assets.logo} alt="" className="w-28 sm:w-32" />
@@ -13,8 +31,9 @@ const Navbar = () => {
           {userData.name[0].toUpperCase()}
           <div className="absolute top-0 right-0 hidden group-hover:block z-10 text-black rounded pt-10 ">
             <ul className="list-none m-0 p-2 bg-gray-100 text-sm">
-              <li className="py-1 px-2 hover:bg-gray-200 cursor-pointer">Verify Email</li>
-              <li className="py-1 px-2 pr-10 hover:bg-gray-200 cursor-pointer">Logout</li>
+              {userData.isVerified ? "" : <li className="py-1 px-2 hover:bg-gray-200 cursor-pointer">Verify Email</li>}
+              
+              <li onClick={handleLogout} className="py-1 px-2 pr-10 hover:bg-gray-200 cursor-pointer">Logout</li>
             </ul>
           </div>
         </div>
