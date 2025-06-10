f81e1b219295bdcb7ea19e4247b5f9c77df40b21
Aman Mishra <amanmishra.5272@gmail.com>
2025-06-11 02:23:07 +0530

    feat: enhance EmailVerify component to handle paste functionality for OTP input

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -17,14 +17,24 @@ const EmailVerify = () => {
     }
   }
 
+  const handlePaste = (e) =>{
+    const paste = e.clipboardData.getData('text');
+    const pasteArray = paste.split('');
+    pasteArray.forEach((char, index)=>{
+      if (inputRefs.current[index]) {
+        inputRefs.current[index].value = char;
+      }
+    })
+  }
+
   return (
     <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to to-purple-400">
       <img src={assets.logo} alt="" className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer" onClick={() => navigate("/")} />
       <form className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
         <h1 className="text-white text-2xl font-semibold text-center mb-4 ">Email Verify OTP</h1>
         <p className="text-center mb-6 text-indigo-300">Enter the 6-digit code send to your email id</p>
-        <div className="flex justify-between mb-8">
-          {Array(5)
+        <div className="flex justify-between mb-8" onPaste={handlePaste}>
+          {Array(6)
             .fill(0)
             .map((_, index) => (
               <input
