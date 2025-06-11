24bbf72a49eab07d373c7aef4d866ba11cb254d3
Aman Mishra <amanmishra.5272@gmail.com>
2025-06-11 18:34:47 +0530

    fix: update sameSite attribute for cookie to 'none' for cross-site compatibility

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -34,7 +34,8 @@ export const register = async (req, res) => {
         res.cookie('token',token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production'? 'none' :'strict',
+            // sameSite: process.env.NODE_ENV === 'none'? 'none' :'strict',
+            sameSite: 'none',
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
